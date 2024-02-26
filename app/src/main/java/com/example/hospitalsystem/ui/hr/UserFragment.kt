d1eceba2219a0703f63d31e6b47b5f3ea062c303
abdul <abdullah.abo.alssaued@gmail.com>
2024-02-26 22:20:06 +0200

    update 3

--- app/src/main/java/com/example/hospitalsystem/ui/hr/UserFragment.kt
+++ app/src/main/java/com/example/hospitalsystem/ui/hr/UserFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.hospitalsystem.R
 import com.example.hospitalsystem.databinding.FragmentUserBinding
-
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 
 class UserFragment : Fragment() {
@@ -27,11 +31,25 @@ class UserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.apply {
+            editDateOfBirth.setOnClickListener {
+                val datePicker =
+                    MaterialDatePicker.Builder.datePicker()
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .setTheme(R.style.Base_Theme_HospitalSystem)
+                        .build()
+
+                datePicker.show(parentFragmentManager,"material date picker")
+                datePicker.addOnPositiveButtonClickListener {
+                    val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                    editDateOfBirth.setText(dateFormat.format(Date(it).time))
+                }
+            }
+        }
 
         binding.btnBack.setOnClickListener {
             findNavController().navigateUp()
         }
-
     }
 
 
